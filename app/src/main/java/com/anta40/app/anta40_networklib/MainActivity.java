367ac422cecc9048883c6d4d90dfa74654a48e28
Andre Tampubolon <andre@lc.vlsm.org>
2019-03-26 13:04:15 +0700

    First commit

---
+++ app/src/main/java/com/anta40/app/anta40_networklib/MainActivity.java
@@ -0,0 +1,13 @@
+package com.anta40.app.anta40_networklib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
